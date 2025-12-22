798071e3626d5fba7bd02336075ec1d7dd1cb349
ropl.btc <github.com.thermal146@passmail.net>
2025-12-22 01:27:50 +0100

    optimized favicon for dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,50 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<link rel="icon" href="/favicon.ico" sizes="any"></link>
-				<link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32"></link>
-				<link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16"></link>
+				<link
+					rel="icon"
+					href="/favicon.png"
+					type="image/png"
+					sizes="118x118"
+					media="(prefers-color-scheme: light)"
+				></link>
+				<link
+					rel="icon"
+					href="/favicon-dark.png"
+					type="image/png"
+					sizes="118x118"
+					media="(prefers-color-scheme: dark)"
+				></link>
+				<link
+					rel="icon"
+					href="/favicon-32x32.png"
+					type="image/png"
+					sizes="32x32"
+					media="(prefers-color-scheme: light)"
+				></link>
+				<link
+					rel="icon"
+					href="/favicon-32x32-dark.png"
+					type="image/png"
+					sizes="32x32"
+					media="(prefers-color-scheme: dark)"
+				></link>
+				<link
+					rel="icon"
+					href="/favicon-16x16.png"
+					type="image/png"
+					sizes="16x16"
+					media="(prefers-color-scheme: light)"
+				></link>
+				<link
+					rel="icon"
+					href="/favicon-16x16-dark.png"
+					type="image/png"
+					sizes="16x16"
+					media="(prefers-color-scheme: dark)"
+				></link>
+				<link rel="icon" href="/favicon.ico" sizes="any" media="(prefers-color-scheme: light)"></link>
+				<link rel="icon" href="/favicon-dark.ico" sizes="any" media="(prefers-color-scheme: dark)"></link>
 				<link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
 				<link rel="manifest" href="/site.webmanifest"></link>
 			</head>
